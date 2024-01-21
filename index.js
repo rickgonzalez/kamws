c7ce1e177de7f51966d657acb746d0f0c5d13500
Rick Gonzalez <rick.gonzalez68@gmail.com>
2024-01-21 17:42:50 -0500

    update index page and express for testing

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 import WebSocket, { WebSocketServer } from 'ws';
 let clients = [];
 createWebSocketServer();
+var express = require('express');
+var app = express();
 
+var options = {
+  index: "coming-soon.html"
+};
+
+app.use('/', express.static('app', options));
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('my app is listening at http://%s:%s', host, port);
+});
 
 export function createWebSocketServer() {
   //this may not be necessary
